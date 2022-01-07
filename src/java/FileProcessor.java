0d94b154338bf740bda6d20a844569f53ce2832e
Darren Tebo <darrentebo83@outlook.com>
2022-01-07 14:42:21 -0500

    added FileProcessor class

---
+++ src/java/FileProcessor.java
@@ -0,0 +1,14 @@
+public class FileProcessor {
+    private String[][] rowsAndColumns;
+
+    public FileProcessor() {
+    }
+
+    public String[][] getRowsAndColumns() {
+        return rowsAndColumns;
+    }
+
+    public void setRowsAndColumns(String[][] rowsAndColumns) {
+        this.rowsAndColumns = rowsAndColumns;
+    }
+}
